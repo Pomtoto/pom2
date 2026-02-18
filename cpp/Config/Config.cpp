b59f1fc2f9f6a9b1c6a4176b3f9dea814bfddeff
Pomtoto <94511458+Pomtoto@users.noreply.github.com>
2026-02-19 02:48:53 +0300

    Update Config.cpp

--- cpp/Config/Config.cpp
+++ cpp/Config/Config.cpp
@@ -1,5 +1,29 @@
 #include "Config.h"
 
-sAutoRetribution AutoRetribution;
-sAim Aim;
-sESP ESP;
+namespace Configs {
+    int EglWidth = 0;
+    int EglHeight = 0;
+    int DisplayWidth = 0;
+    int DisplayHeight = 0;
+    bool LoginValid = false;
+    bool Initialized = false;
+    bool ClearMouse = true;
+    bool ShowImGui = true;
+};
+
+namespace Esp {
+    bool SkillsId = false;
+}
+
+namespace AutoSkills {
+    bool SoYou = false;
+    bool Lucas = false;
+    bool KimmyTrigger = false;
+    bool GusionTrigger = false;
+    bool JoyTrigger = false;
+    bool PaquitoTrigger = false;
+};
+
+sAutoRetribution AutoRetribution{false, false, false, false, 7.0f};
+sAim Aim{false, false, false, false, false, 10.0f, Vector3::zero(), Vector3::One(), Vector3::zero(), 0, 9999, false};
+sESP ESP{false, false, false, false, false, false, false, false, false, false};
